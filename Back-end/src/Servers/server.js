72a451cae64f4394394386b018768c5c603560b0
NourhanRadwan145 <nourshaheen170@gmail.com>
2024-05-05 18:25:58 +0300

    vercel.json

--- Back-end/src/Servers/server.js
+++ Back-end/src/Servers/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors({
     credentials:true,
-    origin:["http://localhost:4200"]
+    origin:["https://ecommerce-furniture-website-1sal0vxhy.vercel.app/"]
 }))
 app.use(cookieParser())
 
